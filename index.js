e054b8841e720dfeb5aeaf8c507cd98c5007ee36
Sammy <samantha.megan10@gmail.com>
2023-01-11 22:38:51 -0700

    convert to hapi

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const resorts = [
-  'https://kickinghorseresort.com/conditions/snow-report/',
-  'https://www.skilouise.com/snow-conditions/',
-  'https://www.skibanff.com/conditions',
-  'https://skifernie.com/conditions/snow-report/',
-  'https://www.skimarmot.com/mountain/weather-conditions/',
-  'https://www.revelstokemountainresort.com/conditions/snow-report/',
-  'https://skiwhitewater.com/conditions/',
-  'https://www.whistlerblackcomb.com/the-mountain/mountain-conditions/snow-and-weather-report.aspx',
-  'https://www.redresort.com/report/',
-];
+'use strict';
 
-for (i = 0; i < resorts.length; i++) {
-  const holder = document.getElementById('holder');
+const Hapi = require('@hapi/hapi');
 
-  var inner = document.createElement('div');
-  inner.setAttribute('class', 'inner');
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-  var x = document.createElement('EMBED');
-  x.setAttribute('src', resorts[i]);
-  x.setAttribute('class', 'embed');
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello World!';
+    },
+  });
 
-  inner.appendChild(x);
-  holder.appendChild(inner);
-}
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
